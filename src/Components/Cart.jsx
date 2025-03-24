d661327f1a698e0cca98dfae0679deddde653b90
Gurkaran017 <gurkaransingh017gh@gmail.com>
2025-03-24 19:55:50 +0530

    Amazon Clone

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import cart from "../assets/Images/cart.svg";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   return (
@@ -17,18 +18,21 @@ const Cart = () => {
           </p>
         </div>
         <div className="flex justify-center ">
-          <button className="bg-[#ffd814] w-[95vw] rounded-full p-3 mt-5">
-            Sign in to your account
-          </button>
+          <Link to="/signIn">
+            <button className="bg-[#ffd814] w-[95vw] rounded-full p-3 mt-5">
+              Sign in to your account
+            </button>
+          </Link>
         </div>
         <div className="flex justify-center ">
+          <Link to="/signIn">
           <button className="border  border-black w-[95vw] rounded-full p-3 mt-2">
             Sign up now
           </button>
+          </Link>
         </div>
 
-        <div className="min-h-10 bg-gray-200 mt-4">
-        </div>
+        <div className="min-h-10 bg-gray-200 mt-4"></div>
         <div className="flex justify-center ">
           <button className="bg-[#ffd814] w-[95vw] rounded-full p-3 mt-5">
             Continue
